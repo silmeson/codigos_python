735fecfa5f55d89a34a6c332f24173309d7ecd81
SilmesonS <silmesonsilmeson@gmail.com>
2024-09-03 09:46:36 -0300

    Calculando a média com python

---
+++ exemplo3.py
@@ -0,0 +1,7 @@
+nota1 = float(input("Informe sua 1ª nota: ")) #convertendo valores textuais em decimais usando o float
+nota2 = float(input("Informe sua 2ª nota: "))
+nota3 = float(input("Informe sua 3ª nota: "))
+
+media = (nota1 + nota2 + nota3)/3
+
+print(f"Sua média é {media:.2f}") #.2f significa que teremos 2 casas decimais após a vírgula
